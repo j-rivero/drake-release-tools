d4443e77dcfdb4ce6c3e2290144628ff99391dd8
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-02-01 13:58:15 +0100

    Run git config to setup email and name

--- lib/config.bash
+++ lib/config.bash
@@ -2,3 +2,6 @@ RELEASE_REPO_URL="https://github.com/j-rivero/drake-release"
 DEBIAN_FRONTEND=noninteractive
 DEBFULLNAME='TRI builder'
 DEBEMAIL='tri@builder.test'
+
+git config --global user.email "${DEBFULLNAME}"
+git config --global user.name "${DEBEMAIL}"
