18a809bde35c4056043f18c3259e9282265f7c77
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-02-07 16:04:55 -0800

    Export pkgs

--- build-drake-pkg.bash
+++ build-drake-pkg.bash
@@ -1,9 +1,12 @@
 IMAGE_NAME="drake-debbuilder:latest"
 
+mkdir -p drake-pkgs
+
 docker build . -t ${IMAGE_NAME} 
 docker run -e TERM=xterm-256color \
     -v /dev/log:/dev/log:ro \
     -v /run/log:/run/log:ro \
+    -v ${PWD}/drake-pkgs:/pkgs \
     --tty \
     --rm \
     ${IMAGE_NAME} \
--- builder/release-new-snapshot.bash
+++ builder/release-new-snapshot.bash
@@ -1,13 +1,91 @@
 #!/bin/bash -e
 
-source "lib/config.bash"
-
+RELEASE_REPO_URL="https://github.com/j-rivero/drake-release"
+DEBIAN_FRONTEND=noninteractive
+DEBFULLNAME='TRI builder'
+DEBEMAIL='tri@builder.test'
 RELEASE_REPO_DIR="$(mktemp -d)/drake-release"
+
+git config --global user.email "${DEBFULLNAME}"
+git config --global user.name "${DEBEMAIL}"
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+error()
+{
+  local msg=${1}
+
+  echo -e "${RED}[EE] ${msg}${NC}"
+  exit 1
+}
+
+info()
+{
+  local msg=${1}
+
+  echo -e "${GREEN}[-] ${msg}${NC}"
+}
+
+install_bazel()
+{
+  if [[ -z `which bazel` ]]; then
+	info "Did not find bazel. Proceed with the installation"
+	sudo apt-get install -y -qq openjdk-8-jdk bash-completion zlib1g-dev
+	# TODO: this piece of code is extracted from the setup/ directory in the drake
+	# main repository. Refactoring that script should help to avoid the duplication
+	dpkg_install_from_wget() {
+		package="$1"
+		version="$2"
+		url="$3"
+		checksum="$4"
+
+		# Skip the install if we're already at the exact version.
+		installed=$(dpkg-query --showformat='${Version}\n' --show "${package}" 2>/dev/null || true)
+		if [[ "${installed}" == "${version}" ]]; then
+		  echo "${package} is already at the desired version ${version}"
+		  return
+		fi
+
+		# If installing our desired version would be a downgrade, ask the user first.
+		if dpkg --compare-versions "${installed}" gt "${version}"; then
+		  echo "This system has ${package} version ${installed} installed."
+		  echo "Drake suggests downgrading to version ${version}, our supported version."
+		  read -r -p "Do you want to downgrade? [Y/n] " reply
+		  if [[ ! "${reply}" =~ ^([yY][eE][sS]|[yY])*$ ]]; then
+			  echo "Skipping ${package} ${version} installation."
+			  return
+		  fi
+		fi
+
+		# Download and verify.
+		tmpdeb="/tmp/${package}_${version}-amd64.deb"
+		wget -O "${tmpdeb}" "${url}"
+		if echo "${checksum} ${tmpdeb}" | sha256sum -c -; then
+		  echo  # Blank line between checkout output and dpkg output.
+		else
+		  die "The ${package} deb does not have the expected SHA256.  Not installing."
+		fi
+
+		# Install.
+		dpkg -i "${tmpdeb}"
+		rm "${tmpdeb}"
+	}
+
+	# Install Bazel.
+	dpkg_install_from_wget \
+		bazel 0.6.1 \
+		https://github.com/bazelbuild/bazel/releases/download/0.6.1/bazel_0.6.1-linux-x86_64.deb \
+		5012d064a6e95836db899fec0a2ee2209d2726fae4a79b08c8ceb61049a115cd
+  fi
+}
 
 clone_release_repo()
 {
   git clone ${RELEASE_REPO_URL} ${RELEASE_REPO_DIR} -q > git_clone.log || error "Failed the clone of ${RELEASE_REPO_URL}"
   pushd ${RELEASE_REPO_DIR} > /dev/null
+  cat debian/rules
   # need upstream branch to make gbp work
   git checkout upstream
   git checkout master
@@ -37,6 +115,7 @@ generate_changelog()
 {
   pushd ${RELEASE_REPO_DIR} > /dev/null
   gbp dch --auto --multimaint-merge --ignore-branch --distribution `lsb_release -c -s` --force-distribution --commit || error "Problem generating the new changelog entry"
+  cat debian/changelog
   popd > /dev/null
 }
 
@@ -54,6 +133,14 @@ build_package()
   popd > /dev/null
 }
 
+export_pkgs()
+{
+  pushd ${RELEASE_REPO_DIR} > /dev/null
+  mv ../*.deb ../*.dsc ../*.xz /pkgs/ || true
+  popd > /dev/null
+}
+
+
 info "Cloning the release repository"
 clone_release_repo
 
@@ -66,8 +153,14 @@ import_snapshot
 info "Generate debian changelogs"
 generate_changelog
 
+info "Installing Bazel"
+install_bazel
+
 info "Install build dependencies"
 install_build_dependencies
 
 info "Build packages"
 build_package
+
+info "Export pkgs"
+export_pkgs
