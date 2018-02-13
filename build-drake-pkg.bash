0204868886f5da4aabd174cdc3ba6a55ebaba2cf
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-02-12 21:02:06 -0800

    Move drake-pkgs directory mapping to docker run

--- build-drake-pkg.bash
+++ build-drake-pkg.bash
@@ -10,6 +10,7 @@ docker build . --no-cache -t ${IMAGE_NAME}
 docker run -e TERM=xterm-256color \
     -v /dev/log:/dev/log:ro \
     -v /run/log:/run/log:ro \
+    -v ${PWD}/drake-pkgs:/pkgs:rw \
     --tty \
     --rm \
     ${IMAGE_NAME} \
