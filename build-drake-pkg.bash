b176043b5ab97463a82f05ea875a70d140e47e8c
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-02-14 11:09:28 -0800

    Pass MAKE_JOBS to docker enviroment

--- build-drake-pkg.bash
+++ build-drake-pkg.bash
@@ -8,6 +8,7 @@ mkdir -p drake-pkgs
 # the development
 docker build . --no-cache -t ${IMAGE_NAME}
 docker run -e TERM=xterm-256color \
+    -e MAKE_JOBS \
     -v /dev/log:/dev/log:ro \
     -v /run/log:/run/log:ro \
     -v ${PWD}/drake-pkgs:/pkgs:rw \
